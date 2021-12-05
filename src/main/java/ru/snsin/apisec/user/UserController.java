0cce218cb4c79484d617dd9bca0fba991864f974
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2021-12-05 21:54:27 +0300

    WIP: create JWT with nimbus

--- src/main/java/ru/snsin/apisec/user/UserController.java
+++ src/main/java/ru/snsin/apisec/user/UserController.java
@@ -1,12 +1,17 @@
 package ru.snsin.apisec.user;
 
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.SecureRandom;
+import java.sql.Date;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Map;
 
@@ -27,7 +32,33 @@ public class UserController {
 
     @PostMapping(value = "/authenticate")
     public Map<String, Object> authenticate(@RequestBody @Valid UserDto user) {
-        return Collections.singletonMap("token",
-                "JWT for %s will be there".formatted(user.getEmail()));
+
+        SecureRandom random = new SecureRandom();
+        byte[] sharedSecret = new byte[32];
+        random.nextBytes(sharedSecret);
+
+        JWSSigner signer;
+        try {
+            signer = new MACSigner(sharedSecret);
+        } catch (KeyLengthException e) {
+            e.printStackTrace();
+            return Collections.singletonMap("error", e.getMessage());
+        }
+        Instant expirationDate = Instant.now().plusSeconds(600);
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("anonymous")
+                .issuer("apisec-app")
+                .expirationTime(Date.from(expirationDate))
+                .claim("roles", Collections.singletonList("USER"))
+                .build();
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+        try {
+            signedJWT.sign(signer);
+        } catch (JOSEException e) {
+            e.printStackTrace();
+            return Collections.singletonMap("error", e.getMessage());
+        }
+
+        return Collections.singletonMap("access_token", signedJWT.serialize());
     }
 }
